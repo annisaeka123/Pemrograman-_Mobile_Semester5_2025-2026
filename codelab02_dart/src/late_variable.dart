a35f2ed4c5e08c8b4c5254e1c10d500fc415be30
annisa eka <annisaekapuspitaaa@gmail.com>
2025-09-04 15:20:39 +0700

    Tugas Praktikum (soal 4)

---
+++ codelab02_dart/src/late_variable.dart
@@ -0,0 +1,9 @@
+
+void main() {
+  late String pesan;
+
+  // Inisialisasi belakangan
+  pesan = "Hello dari Dart!";
+  
+  print(pesan);  // Tidak error karena sudah diinisialisasi sebelum dipakai
+}
---
+++ codelab02_dart/src/null_safety.dart
@@ -0,0 +1,7 @@
+void main() {
+  String nama = "Annisa";     // Wajib ada nilai
+  String? alamat;             // Boleh null (karena ada '?')
+
+  print("Nama: $nama");
+  print("Alamat: $alamat");   // Hasilnya null
+}
