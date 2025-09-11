998d902585bd8b329cb45c46c21cfc0031291697
annisa eka <annisaekapuspitaaa@gmail.com>
2025-09-11 15:53:35 +0700

    Praktikum2

---
+++ codelab03_dart/praktikum2/src/praktikum2.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  // ===LANGKAH1===
+  // while (counter < 33) {
+  //   print(counter);
+  //   counter++;
+  // }
+
+  // ===PERBAIKAN LANGKAH 1===
+  int counter = 0; // deklarasi dan inisialisasi variabel
+
+  while (counter < 33) {
+    print(counter);
+    counter++;
+  }
+
+  // ===LANGKAH 3===
+  do {
+    print(counter);
+    counter++;
+  } while (counter < 77);
+
+}
