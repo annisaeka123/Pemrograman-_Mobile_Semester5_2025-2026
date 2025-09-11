4d604b845e912732db2772a9d6c3793ffa3f30e0
annisa eka <annisaekapuspitaaa@gmail.com>
2025-09-11 15:08:08 +0700

    Praktikum 1

---
+++ codelab03_dart/praktikum1.dart
@@ -0,0 +1,39 @@
+void main(List<String> args) {
+  // ===LANGKAH 1===
+  // String test = "test2";
+  // if (test == "test1") {
+  //   print("Test1");
+  // } else If (test == "test2") {
+  //   print("Test2");
+  // } Else {
+  //   print("Something else");
+  // }
+  // if (test == "test2") print("Test2 again");
+
+  // ===PERBAIKAN LANGKAH 1===
+  // String test = "test2";
+  // if (test == "test1") {
+  //   print("Test1");
+  // } else if (test == "test2") {
+  //   print("Test2");
+  // } else {
+  //   print("Something else");
+  // }
+
+  // if (test == "test2") print("Test2 again");
+
+  // ===LANGKAH 3===
+  // String test = "true";
+  // if (test) {
+  //   print("Kebenaran");
+  // }
+
+  // ===PERBAIKAN LANGKAH 3===
+  String test = "true";
+  if (test == "true") {
+    print("Kebenaran");
+  } else {
+    print("Kesalahan");
+  }
+
+}
