9bab51b7f793d2dc4fc21fa0cf19848c959070da
annisa eka <annisaekapuspitaaa@gmail.com>
2025-10-26 16:33:31 +0700

    praktikum 2

---
+++ codelab08_dart/src/photo_filter_carousel/lib/widget/filter_item.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class FilterItem extends StatelessWidget {
+  const FilterItem({
+    super.key,
+    required this.color,
+    this.onFilterSelected,
+  });
+
+  final Color color;
+  final VoidCallback? onFilterSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onFilterSelected,
+      child: AspectRatio(
+        aspectRatio: 1.0,
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ClipOval(
+            child: Image.network(
+              'https://i.pinimg.com/1200x/6d/23/fe/6d23fee103c7813142567f33604d9f65.jpg',
+              color: color.withOpacity(0.5),
+              colorBlendMode: BlendMode.hardLight,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
