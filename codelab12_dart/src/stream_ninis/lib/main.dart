e6d2b22637627cb9fd15526b46b935f6f847c5db
annisa eka <annisaekapuspitaaa@gmail.com>
2025-11-18 19:32:34 +0700

    W12: Soal 6

--- codelab12_dart/src/stream_ninis/lib/main.dart
+++ codelab12_dart/src/stream_ninis/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'stream.dart';
+import 'dart:async';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -34,9 +36,20 @@ class _StreamHomePageState extends State<StreamHomePage> {
       appBar: AppBar(
         title: const Text('Stream'),
       ),
-      body: Container(
-        decoration: BoxDecoration(color: bgColor),
-      ),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(lastNumber.toString()),
+            ElevatedButton(
+              onPressed: () => addRandomNumber(),
+              child: Text('New Random Number'),
+            ),
+          ],
+        ),
+      )
     );
   }
 
@@ -52,10 +65,39 @@ class _StreamHomePageState extends State<StreamHomePage> {
     });
   }
 
+  // Langkah 8=================
   @override
   void initState() {
+    numberStream = NumberStream();
+    numberStreamController = numberStream.controller;
+    Stream stream = numberStreamController.stream;
+    stream.listen((event) {
+      setState(() {
+        lastNumber = event;
+      });
+    });
     super.initState();
-    colorStream = ColorStream();
-    changeColor();
   }
+  // ==========================
+
+  int lastNumber = 0;
+  late StreamController numberStreamController;
+  late NumberStream numberStream;
+
+  @override
+  void dispose() {
+    numberStreamController.close();
+    super.dispose();
+  }
+
+  // Langkah 10=================
+  void addRandomNumber() {
+    Random random = Random();
+    int myNum = random.nextInt(10);
+    numberStream.addNumberToSink(myNum);
+  }
+  // ==========================
+
 }
+
+
