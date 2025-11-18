061dc0dc77f62091301e953965a2360cdea54ee6
annisa eka <annisaekapuspitaaa@gmail.com>
2025-11-18 16:30:44 +0700

    W12: Jawaban Soal 2

---
+++ codelab12_dart/src/stream_ninis/lib/stream.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> colors = [
+    const Color.fromARGB(255, 61, 140, 180),
+    const Color.fromARGB(255, 255, 223, 129),
+    const Color.fromARGB(255, 199, 166, 255),
+    const Color.fromARGB(255, 140, 219, 255),
+    const Color.fromARGB(255, 253, 188, 255)
+  ];
+}
