8e705d5a2b5bb610558a3b4f628c227369a798b0
annisa eka <annisaekapuspitaaa@gmail.com>
2025-11-18 22:20:46 +0700

    W12: soal 9, Praktikum 4

--- codelab12_dart/src/stream_ninis/lib/main.dart
+++ codelab12_dart/src/stream_ninis/lib/main.dart
@@ -47,6 +47,10 @@ class _StreamHomePageState extends State<StreamHomePage> {
               onPressed: () => addRandomNumber(),
               child: Text('New Random Number'),
             ),
+            ElevatedButton(
+              onPressed: () => stopStream(),
+              child: const Text('Stop Subscription'),
+            )
           ],
         ),
       )
@@ -65,37 +69,29 @@ class _StreamHomePageState extends State<StreamHomePage> {
     });
   }
 
+  // Langkah 2 =================
   @override
   void initState() {
     numberStream = NumberStream();
     numberStreamController = numberStream.controller;
     Stream stream = numberStreamController.stream;
-
-    // Langkah 2=============
-    transformer = StreamTransformer<int, int>.fromHandlers(
-      handleData: (value, sink) {
-        sink.add(value * 10);
-      },
-      handleError: (error, trace, sink) {
-        sink.add(-1);
-      },
-      handleDone: (sink) => sink.close()
-    );
-    // =======================
-
-    // Langkah 3=============
-    stream.transform(transformer).listen((event) {
+    subscription = stream.listen((event) {
       setState(() {
         lastNumber = event;
       });
-    }).onError((error) {
+    });
+    super.initState();
+  // ===========================
+
+    subscription.onError((error) {
       setState(() {
         lastNumber = -1;
       });
     });
-    super.initState();
-    // ======================
 
+    subscription.onDone(() {
+      debugPrint('OnDone was called');
+    });
   }
 
   int lastNumber = 0;
@@ -106,18 +102,29 @@ class _StreamHomePageState extends State<StreamHomePage> {
   void dispose() {
     numberStreamController.close();
     super.dispose();
+    subscription.cancel(); // Langkah 6 
   }
 
+  // Langkah 8 =================
   void addRandomNumber() {
     Random random = Random();
     int myNum = random.nextInt(10);
-    numberStream.addNumberToSink(myNum);
-    // numberStream.addError();
+    if (!numberStreamController.isClosed) {
+      numberStream.addNumberToSink(myNum);
+    } else {
+      setState(() {
+        lastNumber = -1;
+      });
+    }
   }
+  // ===========================
 
-  // Langkah 1  ===========
   late StreamTransformer transformer;
-  // ======================
+  late StreamSubscription subscription;
+
+  void stopStream() {
+    numberStreamController.close();
+  }
 
 }
 
