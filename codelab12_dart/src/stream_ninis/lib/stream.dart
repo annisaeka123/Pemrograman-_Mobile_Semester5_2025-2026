81d8ff9a64d74614156c2502982ad94fa52dd36c
annisa eka <annisaekapuspitaaa@gmail.com>
2025-11-18 16:40:07 +0700

    W12: Jawaban Soal 3

--- codelab12_dart/src/stream_ninis/lib/stream.dart
+++ codelab12_dart/src/stream_ninis/lib/stream.dart
@@ -8,4 +8,12 @@ class ColorStream {
     const Color.fromARGB(255, 140, 219, 255),
     const Color.fromARGB(255, 253, 188, 255)
   ];
+
+  Stream<Color> getColorStream() async* {
+    yield* Stream.periodic(
+      const Duration(seconds: 1), (int t) {
+        int index = t % colors.length;
+        return colors[index];
+    });
+  }
 }
