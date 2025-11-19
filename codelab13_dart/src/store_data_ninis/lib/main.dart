11916fdab88577718844debfbafd5df2f82506c6
annisa eka <annisaekapuspitaaa@gmail.com>
2025-11-19 22:22:53 +0700

    W13: Praktikum 1 (soal 3)

--- codelab13_dart/src/store_data_ninis/lib/main.dart
+++ codelab13_dart/src/store_data_ninis/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import '../model/pizza.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,22 +37,43 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('JSON - Ninis')),
-      body: Text(pizzaString),
+      body: ListView.builder(
+        itemCount: myPizzas.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(myPizzas[index].pizzaName),
+            subtitle: Text(myPizzas[index].description),
+          );
+        },
+      ),
     );
   }
 
-  Future readJsonFile() async {
+  Future<List<Pizza>> readJsonFile() async {
     String myString = await DefaultAssetBundle.of(context)
         .loadString('assets/pizzalist.json');
-    setState(() {
-      pizzaString = myString;
-    });
+
+    List pizzaMapList = jsonDecode(myString);
+
+    List<Pizza> myPizzas = [];
+    for (var pizza in pizzaMapList) {
+      Pizza myPizza = Pizza.fromJson(pizza);
+      myPizzas.add(myPizza);
+    }
+
+    return myPizzas;
   }
 
   @override
   void initState() {
     super.initState();
-    readJsonFile();
+    readJsonFile().then((value) {
+      setState(() {
+        myPizzas = value;
+      });
+    });
   }
+
+  List<Pizza> myPizzas = [];
   
 }
